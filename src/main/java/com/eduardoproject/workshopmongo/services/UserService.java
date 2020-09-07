34e6e49996a9b295f03bbd60220c748401fe01bc
eduardo0lima <55849785+eduardo0lima@users.noreply.github.com>
2020-09-07 16:21:49 -0300

    Insert user with post

--- src/main/java/com/eduardoproject/workshopmongo/services/UserService.java
+++ src/main/java/com/eduardoproject/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.eduardoproject.workshopmongo.domain.User;
+import com.eduardoproject.workshopmongo.dto.UserDTO;
 import com.eduardoproject.workshopmongo.repository.UserRepository;
 import com.eduardoproject.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,5 +26,11 @@ public class UserService {
 		return user.orElseThrow(() -> new ObjectNotFoundException("Object not foud."));
 	}
 	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
 	
+	public User fromDTO(UserDTO objDto) {
+		return new  User(objDto.getId(),objDto.getName(),objDto.getEmail());
+	}
 }
