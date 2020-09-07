2e4fbde6379905d879ee0d5cca8dfbbb0bea3d18
eduardo0lima <55849785+eduardo0lima@users.noreply.github.com>
2020-09-07 17:23:39 -0300

    Referenciando os posts do usuário

--- src/main/java/com/eduardoproject/workshopmongo/domain/User.java
+++ src/main/java/com/eduardoproject/workshopmongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.eduardoproject.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "user")//ou @Document puro
@@ -15,6 +18,8 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {
 		super();
@@ -51,6 +56,14 @@ public class User implements Serializable {
 		this.email = email;
 	}
 
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -75,6 +88,8 @@ public class User implements Serializable {
 			return false;
 		return true;
 	}
+
+	
 	
 		
 }
