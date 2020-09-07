1d1b5aaa10fa138ccb916ce2255db3584ce3108c
eduardo0lima <55849785+eduardo0lima@users.noreply.github.com>
2020-09-07 15:03:21 -0300

    Connecting to MongoDB with repository and service

--- src/main/java/com/eduardoproject/workshopmongo/domain/User.java
+++ src/main/java/com/eduardoproject/workshopmongo/domain/User.java
@@ -2,11 +2,16 @@ package com.eduardoproject.workshopmongo.domain;
 
 import java.io.Serializable;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "user")//ou @Document puro
 public class User implements Serializable {
 
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
 	private String id;
 	private String name;
 	private String email;
---
+++ src/main/java/com/eduardoproject/workshopmongo/services/UserService.java
@@ -0,0 +1,21 @@
+package com.eduardoproject.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eduardoproject.workshopmongo.domain.User;
+import com.eduardoproject.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+	
+}
